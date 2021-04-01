c76c18712d72d1e997f26616089a7be526fa705b
Aditya Wasan <aditya@navanatech.in>
2021-04-01 19:23:42 +0530

    di: add modules and inject api to repositories
    Signed-off-by: Aditya Wasan <aditya@navanatech.in>

--- client/app/src/main/java/com/microsoft/research/karya/data/repo/LanguageRepository.kt
+++ client/app/src/main/java/com/microsoft/research/karya/data/repo/LanguageRepository.kt
@@ -2,8 +2,9 @@ package com.microsoft.research.karya.data.repo
 
 import com.microsoft.research.karya.data.service.LanguageAPI
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class LanguageRepository(private val languageAPI: LanguageAPI) {
+class LanguageRepository @Inject constructor(private val languageAPI: LanguageAPI) {
     fun getLanguages() = flow {
         val response = languageAPI.getLanguages()
         val languages = response.body()
---
+++ client/app/src/main/java/com/microsoft/research/karya/injection/RepositoryModule.kt
@@ -0,0 +1,36 @@
+package com.microsoft.research.karya.injection
+
+import com.microsoft.research.karya.data.repo.LanguageRepository
+import com.microsoft.research.karya.data.repo.MicroTaskRepository
+import com.microsoft.research.karya.data.repo.WorkerRepository
+import com.microsoft.research.karya.data.service.LanguageAPI
+import com.microsoft.research.karya.data.service.MicroTaskAPI
+import com.microsoft.research.karya.data.service.WorkersAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideLanguageRepository(languageAPI: LanguageAPI): LanguageRepository {
+        return LanguageRepository(languageAPI)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMicroTaskRepository(microTaskAPI: MicroTaskAPI): MicroTaskRepository {
+        return MicroTaskRepository(microTaskAPI)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWorkerRepository(workersAPI: WorkersAPI): WorkerRepository {
+        return WorkerRepository(workersAPI)
+    }
+}
