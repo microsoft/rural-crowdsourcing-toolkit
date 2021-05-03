03ab393cead29343875d5fcab5038559991ef617
Vivek Seshadri <visesha@microsoft.com>
2021-05-03 12:09:37 +0530

    Cleaned up the box server to implement the new box server api. Integrated with the new auth module.

--- server/box/src/controllers/KaryaFileController.ts
+++ server/box/src/controllers/KaryaFileController.ts
@@ -1,124 +1,245 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+//
+// Handlers for all the karya file related routes
 
-import * as fs from 'fs';
-import { KaryaFile, BasicModel } from '@karya/common';
-import { getControllerError } from './ControllerErrors';
-import { insertWorkerFile } from '../models/KaryaFileModel';
+import { KaryaMiddleware, KaryaRouteState } from '../KoaContextState';
 import * as HttpResponse from '@karya/http-response';
-import { KaryaHTTPContext } from './KoaContextType';
 import { envGetString } from '@karya/misc-utils';
+import {
+  BasicModel,
+  KaryaFileRecord,
+  LanguageCode,
+  languageCodes,
+  BlobParameters,
+  getBlobName,
+  MicrotaskAssignmentRecord,
+  MicrotaskRecord,
+  KaryaFile,
+} from '@karya/common';
+import fs from 'fs';
+
+export type KaryaFileGetRouteState = KaryaRouteState<{
+  karya_file: KaryaFileRecord;
+}>;
+
+type KaryaFileGetMiddleware = KaryaMiddleware<KaryaFileGetRouteState>;
 
 /**
- * Get the input karya file for a microtask via the microtask assignment ID. The
- * controller first checks if the microtask assignment is indeed assigned to
- * the requesting worker.
- * @param ctx Karya koa context
+ * Check if language assets exist for a given language.
+ * @param ctx Karya request context
  */
-export async function getInputFileForAssignment(ctx: KaryaHTTPContext) {
-  // Get ID from the context
-  const microTaskAssignmentId = ctx.params.id;
+export const checkLanguageAssets: KaryaFileGetMiddleware = async (ctx, next) => {
+  const language_code: LanguageCode = ctx.params.code;
 
-  try {
-    // Retrieve the microtask assignment
-    const mtaRecord = await BasicModel.getSingle('microtask_assignment', {
-      id: microTaskAssignmentId,
-    });
-
-    // Check if the microtask assignment was made for this worker
-    if (ctx.state.current_user.id != mtaRecord.worker_id) {
-      HttpResponse.Unauthorized(ctx, 'Illegal access. Microtask assignment was not for this worker');
-      return;
-    }
-
-    // Get the microtask for the assignment
-    const microtask = await BasicModel.getSingle('microtask', {
-      id: mtaRecord.microtask_id,
-    });
-
-    // If the microtask has no input file, then return
-    if (!microtask.input_file_id) {
-      HttpResponse.BadRequest(ctx, 'Microtask does not have a input file associated with it');
-      return;
-    }
-
-    const fileId = microtask.input_file_id;
-    const fileRecord = await BasicModel.getSingle('karya_file', { id: fileId });
-    const folder = envGetString('LOCAL_FOLDER');
-    const filepath = `${process.cwd()}/${folder}/${fileRecord.container_name}/${fileRecord.name}`;
-
-    ctx.attachment(fileRecord.name);
-    HttpResponse.OK(ctx, fs.createReadStream(filepath));
-  } catch (e) {
-    const message = getControllerError(e);
-    HttpResponse.NotFound(ctx, message);
+  // Check if language code is valid
+  if (!languageCodes.includes(language_code)) {
+    HttpResponse.BadRequest(ctx, 'Missing or invalid language code');
+    return;
   }
-}
+
+  try {
+    // Get language asset file name
+    const blobParams: BlobParameters = {
+      cname: 'language-assets',
+      language_code,
+      ext: 'tgz',
+    };
+    const fileName = getBlobName(blobParams);
+
+    // Set the karya file record
+    ctx.state.karya_file = await BasicModel.getSingle('karya_file', {
+      container_name: 'language-assets',
+      name: fileName,
+    });
+
+    await next();
+  } catch (e) {
+    HttpResponse.NotFound(ctx, 'Requested language asset does not exist');
+    return;
+  }
+};
 
 /**
- * Upload the output file for an assignment. The controller first checks if the
- * microtask assignment is indeed assigned to the requesting worker.
- * @param ctx Karya koa context
+ * Check if input file exists for an assignment and if the user has access to
+ * the assignment.
+ * @param ctx Karya request context
  */
-export async function uploadOutputFileForAssignment(ctx: KaryaHTTPContext) {
-  // Get the ID and file details from the context
-  const mtaID = ctx.params.id;
+export const checkMicrotaskInputFile: KaryaFileGetMiddleware = async (ctx, next) => {
+  const mta_id: string = ctx.params.id;
 
-  let karyaFile: KaryaFile;
+  // Get the microtask assignment
+  let mta: MicrotaskAssignmentRecord;
   try {
-    karyaFile = JSON.parse(ctx.request.body.data);
+    mta = await BasicModel.getSingle('microtask_assignment', { id: mta_id });
+  } catch (e) {
+    HttpResponse.NotFound(ctx, `Assignment with ID '${mta_id}' does not exist`);
+    return;
+  }
+
+  // Check if user is indeed assigned the microtask assignment
+  if (mta.worker_id != ctx.state.entity.id) {
+    HttpResponse.Forbidden(ctx, 'User does not have access to assignment');
+    return;
+  }
+
+  // Get the microtask
+  let mt: MicrotaskRecord;
+  try {
+    mt = await BasicModel.getSingle('microtask', { id: mta.microtask_id });
+  } catch (e) {
+    // TODO: Convert this to internal server error
+    HttpResponse.BadRequest(ctx, 'No microtask for assignment?');
+    return;
+  }
+
+  // Check if microtask has an input file
+  if (!mt.input_file_id) {
+    HttpResponse.NotFound(ctx, 'No input file for microtask');
+    return;
+  }
+
+  try {
+    ctx.state.karya_file = await BasicModel.getSingle('karya_file', { id: mt.input_file_id });
+    await next();
+  } catch (e) {
+    HttpResponse.NotFound(ctx, 'Could not locate input file record');
+    return;
+  }
+};
+
+/**
+ * Get and return a karya file.
+ * @param ctx Karya request context
+ */
+export const getFile: KaryaFileGetMiddleware = async (ctx) => {
+  const local_folder = envGetString('LOCAL_FOLDER');
+  const cname = ctx.state.karya_file.container_name;
+  const fileName = ctx.state.karya_file.name;
+  const filePath = `${process.cwd()}/${local_folder}/${cname}/${fileName}`;
+
+  try {
+    ctx.attachment(fileName);
+    HttpResponse.OK(ctx, fs.createReadStream(filePath));
+  } catch (e) {
+    HttpResponse.NotFound(ctx, 'Requested file is not available');
+  }
+};
+
+export type KaryaFileSubmitRouteState = KaryaRouteState<{
+  karya_file: KaryaFile;
+  filePath: string;
+}>;
+
+type KaryaFileSubmitMiddleware = KaryaMiddleware<KaryaFileSubmitRouteState>;
+
+/**
+ * Verify if request contains a file, and verify its checksum.
+ * @param ctx Karya request context
+ */
+export const verifyFile: KaryaFileSubmitMiddleware = async (ctx, next) => {
+  const { files } = ctx.request;
+  if (!files || !files.file) {
+    HttpResponse.BadRequest(ctx, 'No file attached for upload');
+    return;
+  }
+
+  let karya_file: KaryaFile;
+  try {
+    karya_file = JSON.parse(ctx.request.body.data);
   } catch (e) {
     HttpResponse.BadRequest(ctx, 'Invalid JSON object');
     return;
   }
 
-  const { files } = ctx.request;
-  if (!files || !files.file) {
-    HttpResponse.BadRequest(ctx, 'Need to attach a file to this request');
+  const file = files.file;
+  if (file instanceof Array) {
+    HttpResponse.BadRequest(ctx, 'Cannot upload multiple files in a single request');
     return;
   }
 
-  const file = files.file;
+  // Set file path
+  ctx.state.filePath = file.path;
 
+  // TODO: Compute and verify checksum
+
+  // Set checksum and timestamp
+  ctx.state.karya_file.algorithm = karya_file.algorithm;
+  ctx.state.karya_file.checksum = karya_file.checksum;
+  ctx.state.karya_file.timestamp = karya_file.timestamp;
+  await next();
+};
+
+/**
+ * Submit output file for an assignment
+ * @param ctx Karya request context
+ */
+export const submitOutputFile: KaryaFileSubmitMiddleware = async (ctx, next) => {
+  const mta_id: string = ctx.params.id;
+
+  // Get the microtask assignment
+  let mta: MicrotaskAssignmentRecord;
   try {
-    // Get microtask assignment record
-    const mtaRecord = await BasicModel.getSingle('microtask_assignment', {
-      id: mtaID,
-    });
+    mta = await BasicModel.getSingle('microtask_assignment', { id: mta_id });
+  } catch (e) {
+    HttpResponse.NotFound(ctx, `Assignment with ID '${mta_id}' does not exist`);
+    return;
+  }
 
-    // Check if the assignment was made for this worker
-    if (ctx.state.current_user.id !== mtaRecord.worker_id) {
-      HttpResponse.Unauthorized(ctx, 'Illegal access. Microtask assignment was not for this worker');
-      return;
-    }
+  // Check if user is indeed assigned the microtask assignment
+  if (mta.worker_id != ctx.state.entity.id) {
+    HttpResponse.Forbidden(ctx, 'User does not have access to assignment');
+    return;
+  }
 
-    // Check if this is an update
-    // TODO: This is a temporary fix
-    if (mtaRecord.output_file_id !== null) {
-      const oldRecord = await BasicModel.getSingle('karya_file', {
-        id: mtaRecord.output_file_id,
-      });
-      HttpResponse.OK(ctx, oldRecord);
-      // HttpResponse.BadRequest(ctx, 'Updating output files is not allowed');
-      return;
-    }
+  const blobParams: BlobParameters = {
+    cname: 'microtask-assignment-output',
+    microtask_assignment_id: mta_id,
+    ext: 'tgz',
+  };
 
-    // Insert the record
-    const fileRecord = await insertWorkerFile(
-      ctx.state.current_user.id,
-      karyaFile,
-      {
-        cname: 'microtask-assignment-output',
-        microtask_assignment_id: mtaRecord.id,
-        ext: 'tgz',
-      },
-      // @ts-ignore
-      file.path
-    );
+  ctx.state.karya_file = {
+    ...ctx.state.karya_file,
+    container_name: 'microtask-assignment-output',
+    name: getBlobName(blobParams),
+    creator: 'worker',
+    creator_id: ctx.state.entity.id,
+    in_box: true,
+  };
 
+  await next();
+};
+
+/**
+ * Submit log file for a worker
+ * @param ctx Karya request context
+ */
+export const submitLogFile: KaryaFileSubmitMiddleware = async (ctx, next) => {
+  ctx.state.karya_file = {
+    ...ctx.state.karya_file,
+    container_name: 'worker-logs',
+    creator: 'worker',
+    creator_id: ctx.state.entity.id,
+  };
+
+  await next();
+};
+
+/**
+ * Submit file
+ * @param ctx Karya request context
+ */
+export const submitFile: KaryaFileSubmitMiddleware = async (ctx, next) => {
+  try {
+    const folder = envGetString('LOCAL_FOLDER');
+    const { container_name, name } = ctx.state.karya_file;
+    const destination = `${process.cwd()}/${folder}/${container_name}/${name}`;
+    await fs.promises.copyFile(ctx.state.filePath, destination);
+
+    // TODO: Check if upsert is needed
+    const fileRecord = await BasicModel.insertRecord('karya_file', ctx.state.karya_file);
     HttpResponse.OK(ctx, fileRecord);
   } catch (e) {
-    const message = getControllerError(e);
-    HttpResponse.NotFound(ctx, message);
+    HttpResponse.BadRequest(ctx, 'Could not copy file');
   }
-}
+};
--- server/box/src/models/KaryaFileModel.ts
+++ server/box/src/models/KaryaFileModel.ts
@@ -5,87 +5,8 @@
  * Functions to handle box-local karya files
  */
 
-import { promises as fsp } from 'fs';
 import md5File from 'md5-file';
-import box_id from '../config/box_id';
-import { ChecksumAlgorithm, KaryaFile, KaryaFileRecord, BasicModel } from '@karya/common';
-import { BlobParameters, getBlobName } from '@karya/common';
-import { envGetString } from '@karya/misc-utils';
-
-/**
- * Insert a karya file created by the worker.
- * @param karyaFile Partial karya file record with checksum and algorithm
- * @param blobParams Parameters for the blob where the file will be uploaded
- * @param filepath Path of the file to be uploaded
- */
-export async function insertWorkerFile(
-  worker_id: string,
-  karyaFile: KaryaFile,
-  blobParams: BlobParameters,
-  filepath: string
-): Promise<KaryaFileRecord> {
-  if (!karyaFile.algorithm) {
-    throw new Error('Need checksum with a file upload');
-  }
-
-  // Verify checksum
-  const checksum = await getChecksum(filepath, karyaFile.algorithm);
-  if (checksum !== karyaFile.checksum) {
-    throw new Error('Checksum did not match.');
-  }
-
-  // Fill out the remaining details
-  karyaFile.container_name = blobParams.cname;
-  karyaFile.name = getBlobName(blobParams);
-  karyaFile.creator = 'worker';
-  karyaFile.creator_id = worker_id;
-  karyaFile.box_id = box_id;
-  karyaFile.in_box = true;
-
-  // copy the file to the right location
-  const folder = envGetString('LOCAL_FOLDER');
-  await fsp.copyFile(filepath, `${process.cwd()}/${folder}/${karyaFile.container_name}/${karyaFile.name}`);
-
-  // Insert the record into the db
-  const fileRecord = await BasicModel.insertRecord('karya_file', karyaFile);
-  return fileRecord;
-}
-
-/**
- * Insert a local file into the karya DB
- * @param path Local path to the file
- * @param blobParams Blob parameters
- *
- * @returns inserted karya file record
- */
-export async function insertLocalKaryaFile(blobParams: BlobParameters, filepath: string): Promise<KaryaFileRecord> {
-  const csAlgo: ChecksumAlgorithm = 'md5';
-
-  // Get blob name
-  const blobName = getBlobName(blobParams);
-
-  // Create checksum
-  const checksum = await getChecksum(filepath, csAlgo);
-
-  // Copy file to the appropriate path
-  const folder = envGetString('LOCAL_FOLDER');
-  await fsp.copyFile(filepath, `${process.cwd()}/${folder}/${blobParams.cname}/${blobName}`);
-
-  // Create karya file object
-  const kf: KaryaFile = {
-    name: blobName,
-    box_id,
-    container_name: blobParams.cname,
-    creator: 'box',
-    creator_id: box_id,
-    algorithm: csAlgo,
-    checksum,
-    in_box: true,
-  };
-
-  const insertedRecord = await BasicModel.insertRecord('karya_file', kf);
-  return insertedRecord;
-}
+import { ChecksumAlgorithm } from '@karya/common';
 
 /**
  * Compute the checksum for a file given the path and checksum algorithm
