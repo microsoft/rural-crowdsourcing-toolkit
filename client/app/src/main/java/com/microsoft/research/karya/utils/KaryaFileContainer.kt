550ff4ee3da1782b9e4a7b4ff3945b67b017c51f
Anurag Shukla <shukla.anurag0006@gmail.com>
2021-05-06 21:10:12 +0530

    Decouple KaryaFileContainer

---
+++ client/app/src/main/java/com/microsoft/research/karya/utils/KaryaFileContainer.kt
@@ -0,0 +1,77 @@
+package com.microsoft.research.karya.utils
+
+import com.microsoft.research.karya.injection.qualifier.FilesDirQualifier
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileNotFoundException
+import javax.inject.Inject
+
+/** Helper enum for accessing karya files */
+sealed class KaryaFileContainer(val cname: String, val fileDirPath: String) {
+
+  /** Get the local directory path for a container */
+  fun getDirectory(): String {
+    val dirPath = "$fileDirPath/$cname"
+    val dir = File(dirPath)
+
+    var success = true
+    if(!dir.exists()) {
+      success = dir.mkdirs()
+    }
+
+    if (success) {
+      return dirPath
+    } else {
+      throw FileNotFoundException()
+    }
+  }
+
+  /** get the blob name for a particular container given the parameters */
+  abstract fun getBlobName(vararg params: String): String
+}
+
+class LANG_RES (fileDirPath: String): KaryaFileContainer("lang-res", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val lrId = params[0]
+    val languageId = params[1]
+    val ext = "m4a"
+    return "$lrId-$languageId.$ext"
+  }
+}
+class L_LRVS (fileDirPath: String): KaryaFileContainer("l-lrvs", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val languageId = params[0]
+    val ext = "tar"
+    return "L-$languageId.$ext"
+  }
+}
+class LR_LRVS (fileDirPath: String): KaryaFileContainer("lr-lrvs", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val lrId = params[0]
+    val ext = "tar"
+    return "LR-$lrId.$ext"
+  }
+}
+class MICROTASK_INPUT (fileDirPath: String): KaryaFileContainer("microtask-input", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val microtaskId = params[0]
+    val ext = "tgz"
+    return "$microtaskId.$ext"
+  }
+}
+class MICROTASK_ASSIGNMENT_OUTPUT (fileDirPath: String): KaryaFileContainer("microtask-assignment-output", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val assignmentId = params[0]
+    val ext = "tgz"
+    return "$assignmentId.$ext"
+  }
+}
+class WORKER_LOGS (fileDirPath: String): KaryaFileContainer("worker-logs", fileDirPath) {
+  override fun getBlobName(vararg params: String): String {
+    val workerId = params[0]
+    val timestamp = params[1]
+    val ext = "gz"
+    return "$workerId-$timestamp.$ext"
+  }
+};
--- client/app/src/main/java/com/microsoft/research/karya/utils/extensions/KaryaFileExtensions.kt
+++ client/app/src/main/java/com/microsoft/research/karya/utils/extensions/KaryaFileExtensions.kt
@@ -2,18 +2,19 @@ package com.microsoft.research.karya.utils.extensions
 
 import android.content.Context
 import com.microsoft.research.karya.ui.base.BaseActivity
+import com.microsoft.research.karya.utils.KaryaFileContainer
 
-fun Context.getContainerDirectory(container: BaseActivity.KaryaFileContainer): String {
-  return container.getDirectory(this)
+fun Context.getContainerDirectory(container: KaryaFileContainer): String {
+  return container.getDirectory()
 }
 
 /** Get the blob name for a [container] with specific [params] */
-fun Context.getBlobName(container: BaseActivity.KaryaFileContainer, vararg params: String): String {
+fun Context.getBlobName(container: KaryaFileContainer, vararg params: String): String {
   return container.getBlobName(*params)
 }
 
 /** Get the blob path for a blob in the [container] with specific [params] */
-fun Context.getBlobPath(container: BaseActivity.KaryaFileContainer, vararg params: String): String {
+fun Context.getBlobPath(container: KaryaFileContainer, vararg params: String): String {
   val dir = getContainerDirectory(container)
   val name = getBlobName(container, *params)
   return "$dir/$name"
