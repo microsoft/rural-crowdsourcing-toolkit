3b891162481912865b6823403471b6f72018616d
Anurag Shukla <shukla.anurag0006@gmail.com>
2021-05-07 11:58:43 +0530

    Add convert to tarball feature

--- client/app/src/main/java/com/microsoft/research/karya/ui/dashboard/DashboardViewModel.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/dashboard/DashboardViewModel.kt
@@ -3,15 +3,24 @@ package com.microsoft.research.karya.ui.dashboard
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
 import com.microsoft.research.karya.data.manager.AuthManager
+import com.microsoft.research.karya.data.model.karya.ChecksumAlgorithm
 import com.microsoft.research.karya.data.model.karya.modelsExtra.TaskInfo
 import com.microsoft.research.karya.data.model.karya.modelsExtra.TaskStatus
+import com.microsoft.research.karya.data.remote.request.UploadFileRequest
 import com.microsoft.research.karya.data.repo.AssignmentRepository
-import com.microsoft.research.karya.data.repo.MicroTaskRepository
+import com.microsoft.research.karya.data.repo.KaryaFileRepository
 import com.microsoft.research.karya.data.repo.TaskRepository
+import com.microsoft.research.karya.injection.qualifier.FilesDirQualifier
+import com.microsoft.research.karya.utils.FileUtils.createTarBall
+import com.microsoft.research.karya.utils.FileUtils.getMD5Digest
+import com.microsoft.research.karya.utils.KaryaFileContainer
+import com.microsoft.research.karya.utils.MICROTASK_ASSIGNMENT_OUTPUT
 import com.microsoft.research.karya.utils.Result
+import com.microsoft.research.karya.utils.extensions.getBlobPath
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,6 +30,11 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
+import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel
@@ -28,9 +42,12 @@ class DashboardViewModel
 constructor(
   private val taskRepository: TaskRepository,
   private val assignmentRepository: AssignmentRepository,
-  private val microTaskRepository: MicroTaskRepository,
+  private val karyaFileRepository: KaryaFileRepository,
+  @FilesDirQualifier private val fileDirPath: String,
   private val authManager: AuthManager,
 ) : ViewModel() {
+
+  val microtaskOutputContainer = MICROTASK_ASSIGNMENT_OUTPUT(fileDirPath)
 
   private val taskInfoComparator =
     compareByDescending<TaskInfo> { taskInfo -> taskInfo.taskStatus.completedMicrotasks }.thenBy { taskInfo ->
@@ -45,7 +62,8 @@ constructor(
     viewModelScope.launch {
       val idToken = authManager.fetchLoggedInWorkerIdToken()
 
-      assignmentRepository.getAssignments(idToken, "new", "").catch { Log.d("dashboard", it.message!!) }.collect()
+      assignmentRepository.getAssignments(idToken, "new", "")
+        .catch { Log.d("dashboard", it.message!!) }.collect()
     }
   }
 
@@ -53,13 +71,62 @@ constructor(
     viewModelScope.launch {
       val updates = assignmentRepository.getLocalCompletedAssignments()
 
+      val filteredAssignments = updates.filter {
+        // output_file_id is the id of the file in the blob storage(cloud) and will be non-empty if the file was already uploaded
+        it.output_file_id == null && it.output.get("files").asJsonArray.size() > 0
+      }
+
+      for ((index, assignment) in filteredAssignments.withIndex()) {
+        val assignmentTarBallPath = getBlobPath(microtaskOutputContainer, assignment.id)
+        val tarBallName = microtaskOutputContainer.getBlobName(assignment.id)
+        val outputDir = microtaskOutputContainer.getDirectory()
+        val fileNames = assignment.output.get("files").asJsonArray.map { it.asString }
+        val outputFilePaths = fileNames.map {
+          "$outputDir/${it}"
+        }
+        createTarBall(assignmentTarBallPath, outputFilePaths, fileNames)
+
+        val idToken = authManager.fetchLoggedInWorkerIdToken()
+        val requestFile = RequestBody.create(
+          "application/tgz".toMediaTypeOrNull(),
+          File(assignmentTarBallPath)
+        )
+        val filePart =
+          MultipartBody.Part.createFormData(
+            "file",
+            tarBallName,
+            requestFile
+          )
+
+        val md5sum = getMD5Digest(assignmentTarBallPath)
+        val uploadFileRequest =
+          UploadFileRequest(
+            0, // TODO: ASK WHY WE NEED BOX_ID
+            microtaskOutputContainer.cname,
+            tarBallName,
+            ChecksumAlgorithm.md5.toString(),
+            md5sum
+          )
+
+        val dataPart = MultipartBody.Part.createFormData(
+          "data", Gson().toJson(uploadFileRequest)
+        )
+
+//        karyaFileRepository.uploadKaryaFile(
+//          idToken,
+//          dataPart,
+//          filePart
+//        )
+
+      }
+
       val idToken = authManager.fetchLoggedInWorkerIdToken()
 
-      assignmentRepository.submitAssignments(idToken, updates)
-        .onEach { assignmentIds ->
-          assignmentRepository.markMicrotaskAssignmentsSubmitted(assignmentIds)
-        }
-        .catch { Log.d("dashboard submit task", it.message!!) }.collect()
+//      assignmentRepository.submitAssignments(idToken, updates)
+//        .onEach { assignmentIds ->
+//          assignmentRepository.markMicrotaskAssignmentsSubmitted(assignmentIds)
+//        }
+//        .catch { Log.d("dashboard submit task", it.message!!) }.collect()
     }
     // TODO: Pass error message to UI
   }
@@ -89,7 +156,14 @@ constructor(
 
         taskList.forEach { taskRecord ->
           val taskStatus = fetchTaskStatus(taskRecord.id)
-          taskInfoList.add(TaskInfo(taskRecord.id, taskRecord.name, taskRecord.scenario_id.toString(), taskStatus))
+          taskInfoList.add(
+            TaskInfo(
+              taskRecord.id,
+              taskRecord.name,
+              taskRecord.scenario_id.toString(),
+              taskStatus
+            )
+          )
         }
 
         val success = DashboardUiState.Success(taskInfoList.sortedWith(taskInfoComparator))
