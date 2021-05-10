8b7a432b31bcb8112f2ebbac360a5ebe4e51e5e8
Vivek Seshadri <visesha@microsoft.com>
2021-05-10 17:39:21 +0530

    Tracking sent_to_server_at to track when assigned tasks have been sent to the server

--- server/core/schema/specs/KaryaDb.ts
+++ server/core/schema/specs/KaryaDb.ts
@@ -217,6 +217,7 @@ const karyaDb: DatabaseSpec<KaryaTableName, KaryaString, KaryaObject> = {
       columns: [
         ['microtask_id', ['>', 'microtask'], 'not unique', 'not nullable', 'not mutable'],
         ['worker_id', ['>', 'worker'], 'not unique', 'not nullable', 'not mutable'],
+        ['sent_to_server_at', ['timestamp'], 'not unique', 'not nullable', 'mutable'],
         ['deadline', ['timestamp'], 'not unique', 'nullable', 'mutable'],
         ['status', ['string', 16, 'MicrotaskAssignmentStatus'], 'not unique', 'not nullable', 'mutable'],
         ['completed_at', ['timestamp'], 'not unique', 'nullable', 'mutable'],
