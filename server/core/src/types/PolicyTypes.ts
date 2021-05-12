7220c07b8db2218eb972a45aad8f9ad2b50d7806
Vivek Seshadri <visesha@microsoft.com>
2021-05-12 23:03:34 +0530

    Removing unnecessary files

--- server/core/src/types/PolicyTypes.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-//
-// Types related to policy. Policy names and maps
-
-export const policyNames = ['n-total'] as const;
-export type PolicyName = typeof policyNames[number];
