12a34ec5089dffe37e0417ee1fc84c7bb2d5495b
JahanviNShah <jahanvishah0309@gmail.com>
2021-05-24 12:37:48 +0530

    Changed toolbar

--- client/app/src/main/java/com/microsoft/research/karya/ui/views/KaryaToolbar.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/views/KaryaToolbar.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.util.AttributeSet
 import androidx.appcompat.widget.Toolbar
 import com.microsoft.research.karya.R
+import com.microsoft.research.karya.databinding.AppToolbar2Binding
 import com.microsoft.research.karya.databinding.AppToolbarBinding
 import com.microsoft.research.karya.utils.extensions.visible
 
@@ -14,15 +15,15 @@ class KaryaToolbar : Toolbar {
   constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
   constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-  private lateinit var binding: AppToolbarBinding
+  private lateinit var binding:AppToolbar2Binding
 
   init {
     initView(context)
   }
 
   private fun initView(context: Context) {
-    val view = inflate(context, R.layout.app_toolbar, this)
-    binding = AppToolbarBinding.bind(view)
+    val view = inflate(context, R.layout.app_toolbar_2, this)
+    binding = AppToolbar2Binding.bind(view)
   }
 
   fun setTitle(title: String) {
