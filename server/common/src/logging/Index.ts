10794fea76568d620cfeb9162387661eb81eb7f0
Vivek Seshadri <visesha@microsoft.com>
2021-07-14 09:56:33 +0530

    Added a common logger module for backend and box

---
+++ server/common/src/logging/Index.ts
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+// Entry point for logging
+
+import { v4 as uuidv4 } from 'uuid';
+import Application from 'koa';
+
+import { newLogger } from '@karya/logger';
+import { envGetString } from '@karya/misc-utils';
+import { InternalError } from '@karya/http-response';
+import { Logger } from '@karya/logger/node_modules/winston';
+
+// Get the logger configuration
+const logFolder = envGetString('LOG_FOLDER');
+const logFolderPath = `${process.cwd()}/${logFolder}`;
+const datePattern = envGetString('LOG_ARCHIVE_PATTERN');
+
+// Http request logger
+const requestLogger = newLogger({
+  name: 'httpRequests',
+  folder: logFolderPath,
+  datePattern,
+});
+
+// Http error logger
+const requestErrorLogger = newLogger({
+  name: 'httpErrors',
+  folder: logFolderPath,
+  datePattern,
+});
+
+/**
+ * Log an HTTP request.
+ * @param ctx Request context
+ * @param next Next middleware to call in the chain
+ */
+export const httpRequestLogger: Application.Middleware = async (ctx, next) => {
+  // Get a unique ID for the request
+  const id = uuidv4();
+
+  // Log the request
+  const request = {
+    id,
+    method: ctx.method,
+    path: ctx.path,
+    query: ctx.request.query,
+    size: ctx.request.length,
+  };
+  requestLogger.info(request);
+
+  // Get start time
+  const start = Date.now();
+
+  // Process the request
+  await next();
+
+  // Get end time
+  const end = Date.now();
+
+  // Log the response
+  const response = {
+    id,
+    status: ctx.status,
+    size: ctx.response.length,
+    time: end - start,
+  };
+  requestLogger.info(response);
+
+  // If failed request, log more details
+  if (ctx.status >= 400) {
+    requestErrorLogger.error({ request, status: ctx.status, message: ctx.response.body });
+  }
+};
+
+/**
+ * Catch any uncaught exceptions in the middleware chain
+ * @param ctx Request context
+ * @param next Next middleware to call
+ */
+export const catchAll: Application.Middleware = async (ctx, next) => {
+  try {
+    await next();
+  } catch (e) {
+    InternalError(ctx, JSON.stringify(e));
+  }
+};
+
+/**
+ * Main logger to be used by the server
+ */
+export const logger: Logger = newLogger({
+  name: 'main',
+  folder: logFolderPath,
+  datePattern,
+  logToConsole: true,
+  consoleLogLevel: 'info',
+});
