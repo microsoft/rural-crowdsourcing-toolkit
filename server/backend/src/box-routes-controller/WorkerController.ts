af69d19b29b25cb51454d0f4f0844141f3b6d60b
Vivek Seshadri <visesha@microsoft.com>
2021-07-31 20:39:44 +0530

    Update worker phone number and reg mechanism on profile update

--- server/backend/src/box-routes-controller/WorkerController.ts
+++ server/backend/src/box-routes-controller/WorkerController.ts
@@ -51,11 +51,11 @@ export const updateWorkers: BoxRouteMiddleware = async (ctx) => {
 
   await BBPromise.mapSeries(workers, async (worker) => {
     try {
-      const { year_of_birth, gender, full_name, language } = worker;
+      const { phone_number, reg_mechanism, year_of_birth, gender, full_name, language } = worker;
       await BasicModel.updateSingle(
         'worker',
         { id: worker.id },
-        { year_of_birth, gender, full_name, language, sent_to_server_at }
+        { phone_number, reg_mechanism, year_of_birth, gender, full_name, language, sent_to_server_at }
       );
       response.push({ id: worker.id, sent_to_server_at });
     } catch (e) {
