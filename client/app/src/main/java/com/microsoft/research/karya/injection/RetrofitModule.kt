29b72a47df4b969609fbc742cc758ba1698c2431
Vivek Seshadri <visesha@microsoft.com>
2021-08-06 19:08:31 +0530

    Setting right box for ai4b speech release

--- client/app/src/main/java/com/microsoft/research/karya/injection/RetrofitModule.kt
+++ client/app/src/main/java/com/microsoft/research/karya/injection/RetrofitModule.kt
@@ -30,7 +30,7 @@ class RetrofitModule {
   @Reusable
   @BaseUrl
   fun provideBaseUrl(): String {
-    return "https://ai4bkaryabox002.eastus.cloudapp.azure.com"
+    return "https://ai4bkaryabox003.eastus.cloudapp.azure.com"
   }
 
   @Provides
