bdb5153cfa0c56570d6b286327559c201a2e89d1
Ananya Saxena <ananya.saxena9716@gmail.com>
2021-08-11 21:35:00 +0530

    Fixed bug in task summary backend function

--- server/common/src/db/models/TaskModel.ts
+++ server/common/src/db/models/TaskModel.ts
@@ -10,8 +10,8 @@ export async function tasksSummary(): Promise<any[]> {
   const response = await knex.raw(`
     SELECT
       task_id,
-      COALESCE(SUM((status='ASSIGNED')::int), 0) as assigned,
-      COALESCE(SUM((status='COMPLETED')::int), 0) as completed,
+      COALESCE(SUM((status='ASSIGNED' OR status='COMPLETED' OR status='VERIFIED')::int), 0) as assigned,
+      COALESCE(SUM((status='COMPLETED' OR status='VERIFIED')::int), 0) as completed,
       COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
       COALESCE(SUM(credits), 0) as cost
     FROM
