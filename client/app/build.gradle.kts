e59ac5c2cd3844d707c3fcd29d648cf237f4fbf2
Vivek Seshadri <visesha@microsoft.com>
2021-08-18 18:15:13 +0530

    Increased version number

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdkVersion(21)
     targetSdkVersion(30)
     multiDexEnabled = true
-    versionCode = 24
+    versionCode = 25
     versionName = "1"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables.useSupportLibrary = true
