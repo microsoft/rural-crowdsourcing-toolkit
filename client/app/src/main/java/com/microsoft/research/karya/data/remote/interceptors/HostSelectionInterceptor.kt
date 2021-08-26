4c6bc52d6d05b85915ccc7953582da08b48a55d6
Anurag Shukla <shukla.anurag0006@gmail.com>
2021-08-26 18:29:43 +0530

    Replace Host in Interceptor

--- client/app/src/main/java/com/microsoft/research/karya/data/remote/interceptors/HostSelectionInterceptor.kt
+++ client/app/src/main/java/com/microsoft/research/karya/data/remote/interceptors/HostSelectionInterceptor.kt
@@ -2,21 +2,23 @@ package com.microsoft.research.karya.data.remote.interceptors
 
 import com.microsoft.research.karya.data.manager.BaseUrlManager
 import kotlinx.coroutines.runBlocking
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.net.URL
 
 class HostSelectionInterceptor(val baseUrlManager: BaseUrlManager): Interceptor {
-  // The URL to which the request will be sent
-  private lateinit var BASE_URL: String
 
   override fun intercept(chain: Interceptor.Chain): Response {
     val request = chain.request()
     val newRequestBuilder = request.newBuilder()
     val newRequest = runBlocking {
       val baseUrl = baseUrlManager.getBaseUrl()
-      newRequestBuilder.url(baseUrl).build()
+      val newUrl = request.url.toString().replace(request.url.host, URL(baseUrl).host)
+      newRequestBuilder
+        .url(newUrl)
+        .build()
     }
-
     return chain.proceed(newRequest)
   }
 }
