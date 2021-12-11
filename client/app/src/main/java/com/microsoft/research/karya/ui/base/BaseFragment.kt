a98ecf746a915d8cfd65c88576e0753a4901c1ac
Vivek Seshadri <visesha@microsoft.com>
2021-12-11 12:18:46 +0530

    Added support to change language via toolbar button

--- client/app/src/main/java/com/microsoft/research/karya/ui/base/BaseFragment.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/base/BaseFragment.kt
@@ -8,14 +8,28 @@ import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.microsoft.research.karya.R
 import com.microsoft.research.karya.data.exceptions.KaryaException
+import com.microsoft.research.karya.data.manager.AuthManager
+import com.microsoft.research.karya.data.model.karya.enums.LanguageType
+import com.microsoft.research.karya.data.repo.WorkerRepository
 import com.microsoft.research.karya.ui.assistant.Assistant
 import com.microsoft.research.karya.ui.assistant.AssistantFactory
+import com.microsoft.research.karya.ui.views.KaryaToolbar
+import com.microsoft.research.karya.utils.extensions.finish
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 abstract class BaseFragment : Fragment {
 
   constructor() : super()
   constructor(@LayoutRes contentLayoutId: Int) : super(contentLayoutId)
+
+  @Inject
+  lateinit var authManagerBase: AuthManager
+
+  @Inject
+  lateinit var workerRepositoryBase: WorkerRepository
 
   @Inject
   lateinit var assistantFactory: AssistantFactory
@@ -30,6 +44,12 @@ abstract class BaseFragment : Fragment {
     return super.onCreateView(inflater, container, savedInstanceState)
   }
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    val toolbar = this.view?.findViewById<KaryaToolbar>(R.id.appTb)
+    toolbar?.setLanguageUpdater { l -> updateUserLanguage(l) }
+  }
+
   /**
    * Get message corresponding to an exception. If it is a Karya exception, then it will have a
    * resource ID that we can use to the get the appropriate message.
@@ -39,6 +59,24 @@ abstract class BaseFragment : Fragment {
     return when (throwable) {
       is KaryaException -> throwable.getMessage(context)
       else -> throwable.message ?: context.getString(R.string.unknown_error)
+    }
+  }
+
+  private fun updateUserLanguage(lang: LanguageType) {
+    CoroutineScope(Dispatchers.IO).launch {
+      try {
+        val worker = authManagerBase.getLoggedInWorker()
+        workerRepositoryBase.updateLanguage(worker.id, lang.toString())
+        CoroutineScope(Dispatchers.Main).launch {
+          val intent = activity?.intent
+          if (intent != null) {
+            finish()
+            startActivity(intent)
+          }
+        }
+      } catch (e: Exception) {
+        // Ignore exceptions
+      }
     }
   }
 }
--- client/app/src/main/java/com/microsoft/research/karya/ui/views/KaryaToolbar.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/views/KaryaToolbar.kt
@@ -3,11 +3,14 @@
 
 package com.microsoft.research.karya.ui.views
 
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.Bitmap
 import android.util.AttributeSet
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 import com.microsoft.research.karya.R
+import com.microsoft.research.karya.data.model.karya.enums.LanguageType
 import com.microsoft.research.karya.databinding.AppToolbarBinding
 import com.microsoft.research.karya.utils.extensions.gone
 import com.microsoft.research.karya.utils.extensions.visible
@@ -27,6 +30,7 @@ class KaryaToolbar : Toolbar {
   private lateinit var binding: AppToolbarBinding
   private var hideLanguage: Boolean = false
   private var titleText: String = ""
+  private lateinit var languageUpdater: (l: LanguageType) -> Unit
 
   init {
     initView(context)
@@ -42,6 +46,10 @@ class KaryaToolbar : Toolbar {
   private fun initView(context: Context) {
     val view = inflate(context, R.layout.app_toolbar, this)
     binding = AppToolbarBinding.bind(view)
+
+    binding.languageName.setOnClickListener {
+      showUpdateLanguageDialog(context)
+    }
   }
 
   override fun onFinishInflate() {
@@ -86,6 +94,44 @@ class KaryaToolbar : Toolbar {
     binding.assistantCv.apply {
       visible()
       setOnClickListener { onClick() }
+    }
+  }
+
+  private fun showUpdateLanguageDialog(context: Context) {
+    val languageDialog = inflate(context, R.layout.dialog_update_language, null)
+    val dialogBuilder = AlertDialog.Builder(context)
+    dialogBuilder.setView(languageDialog)
+    val dialog = dialogBuilder.create()
+    dialog.show()
+
+    languageDialog.findViewById<Button>(R.id.languageEnglish).setOnClickListener {
+      updateUserLanguage(LanguageType.EN)
+      dialog.cancel()
+    }
+
+    languageDialog.findViewById<Button>(R.id.languageHindi).setOnClickListener {
+      updateUserLanguage(LanguageType.HI)
+      dialog.cancel()
+    }
+
+    languageDialog.findViewById<Button>(R.id.languageBengali).setOnClickListener {
+      updateUserLanguage(LanguageType.BN)
+      dialog.cancel()
+    }
+
+    languageDialog.findViewById<Button>(R.id.languageMalayalam).setOnClickListener {
+      updateUserLanguage(LanguageType.ML)
+      dialog.cancel()
+    }
+  }
+
+  fun setLanguageUpdater(updater: (l: LanguageType) -> Unit) {
+    languageUpdater = updater
+  }
+
+  private fun updateUserLanguage(lang: LanguageType) {
+    if (::languageUpdater.isInitialized) {
+      languageUpdater(lang)
     }
   }
 }
