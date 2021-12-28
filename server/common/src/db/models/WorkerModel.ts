e3086a2b8237cc75cfe1b3cc32b2412921ef6f2a
Anurag Shukla <shukla.anurag0006@gmail.com>
2021-12-28 19:02:15 +0530

    Update WorkerModel

--- server/common/src/db/models/WorkerModel.ts
+++ server/common/src/db/models/WorkerModel.ts
@@ -93,6 +93,17 @@ export async function workersTaskSummary(task_id: string): Promise<any[]> {
  }
 
  /**
+  * Get total amount spent by a worker
+  */
+  export async function getTotalSpent(worker_id: string): Promise<number> {
+    const response = await knex.raw(`SELECT sum(amount) as total
+    FROM payments_transaction WHERE worker_id = ${worker_id} 
+    AND status IN ('created', 'queued', 'processing', 'processed')`)
+    let spent = response.rows[0].total
+    return spent? spent: 0
+   }
+
+ /**
   * 
   * @returns eligible workers who can get paid with respective amount
   */
@@ -101,9 +112,9 @@ export async function workersTaskSummary(task_id: string): Promise<any[]> {
     SELECT tw.*, t3.amount FROM 
     (SELECT t2.worker_id, t2.sac-COALESCE(t1.sat,0) as amount FROM (SELECT worker_id, sum(amount)
      AS SAT FROM payments_transaction  WHERE status IN 
-     ('created', 'queued', 'processing', 'processed', 'failed_at_karya') GROUP BY worker_id  ) t1 
-     RIGHT JOIN (SELECT worker_id,sum(credits) AS SAC FROM microtask_assignment WHERE status='VERIFIED' GROUP BY worker_id) 
-     t2 ON (t1.worker_id = t2.worker_id)) t3 INNER JOIN (select * from worker where payments_active=true) tw ON (t3.worker_id=tw.id) 
+     ('created', 'queued', 'processing', 'processed') GROUP BY worker_id  ) t1 
+     RIGHT JOIN (SELECT worker_id,sum(credits) AS SAC FROM microtask_assignment WHERE status='VERIFIED' GROUP BY worker_id) t2
+     ON (t1.worker_id = t2.worker_id)) t3 INNER JOIN (select * from worker where payments_active=true) tw ON (t3.worker_id=tw.id) 
      INNER JOIN (SELECT * FROM payments_account WHERE STATUS='VERIFIED') ta ON (tw.selected_account=ta.id);
    `)
 
