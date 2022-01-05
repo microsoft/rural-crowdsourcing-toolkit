e6daf93868726b1b16fc433e2167051205180f38
Aditya Wasan <aditya@navanatech.in>
2022-01-05 18:46:56 +0530

    feat(payment): update dagger modules
    Signed-off-by: Aditya Wasan <aditya@navanatech.in>

--- client/app/src/main/java/com/microsoft/research/karya/injection/RetrofitModule.kt
+++ client/app/src/main/java/com/microsoft/research/karya/injection/RetrofitModule.kt
@@ -7,6 +7,7 @@ import com.microsoft.research.karya.data.repo.AuthRepository
 import com.microsoft.research.karya.data.service.KaryaFileAPI
 import com.microsoft.research.karya.data.service.LanguageAPI
 import com.microsoft.research.karya.data.service.MicroTaskAssignmentAPI
+import com.microsoft.research.karya.data.service.PaymentAPI
 import com.microsoft.research.karya.data.service.WorkerAPI
 import com.microsoft.research.karya.injection.qualifier.BaseUrl
 import com.microsoft.research.karya.injection.qualifier.KaryaOkHttpClient
@@ -25,11 +26,11 @@ import java.util.concurrent.TimeUnit
 @InstallIn(SingletonComponent::class)
 class RetrofitModule {
 
-  @Provides
-  @Reusable
-  fun provideGsonConverterFactory(): GsonConverterFactory {
-    return GsonConverterFactory.create()
-  }
+    @Provides
+    @Reusable
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
 
   @Provides
   @Reusable
@@ -38,76 +39,84 @@ class RetrofitModule {
     return "https://karyaboxtest.eastus.cloudapp.azure.com"
   }
 
-  @Provides
-  @Reusable
-  fun provideLoggingInterceptor(): HttpLoggingInterceptor {
-    return HttpLoggingInterceptor()
-      .setLevel(HttpLoggingInterceptor.Level.BODY)
-  }
+    @Provides
+    @Reusable
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
 
-  @Provides
-  @Reusable
-  fun provideIdTokenRenewInterceptor(
-    authRepository: AuthRepository,
-    authManager: AuthManager,
-    @BaseUrl baseUrl: String
-  ): IdTokenRenewInterceptor {
-    return IdTokenRenewInterceptor(authRepository, authManager, baseUrl)
-  }
+    @Provides
+    @Reusable
+    fun provideIdTokenRenewInterceptor(
+        authRepository: AuthRepository,
+        authManager: AuthManager,
+        @BaseUrl baseUrl: String
+    ): IdTokenRenewInterceptor {
+        return IdTokenRenewInterceptor(authRepository, authManager, baseUrl)
+    }
 
-  @Provides
-  @Reusable
-  fun provideVersionInterceptor(): VersionInterceptor {
-    return VersionInterceptor()
-  }
+    @Provides
+    @Reusable
+    fun provideVersionInterceptor(): VersionInterceptor {
+        return VersionInterceptor()
+    }
 
-  @KaryaOkHttpClient
-  @Provides
-  @Reusable
-  fun provideOkHttp(
-    idTokenRenewInterceptor: IdTokenRenewInterceptor,
-    versionInterceptor: VersionInterceptor
-  ): OkHttpClient {
-    return OkHttpClient.Builder()
-      .connectTimeout(10, TimeUnit.MINUTES)
-      .readTimeout(10, TimeUnit.MINUTES)
-      .addInterceptor(idTokenRenewInterceptor)
-      .addInterceptor(versionInterceptor)
-      .build()
-  }
+    @KaryaOkHttpClient
+    @Provides
+    @Reusable
+    fun provideOkHttp(
+        idTokenRenewInterceptor: IdTokenRenewInterceptor,
+        versionInterceptor: VersionInterceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.MINUTES)
+            .readTimeout(10, TimeUnit.MINUTES)
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(idTokenRenewInterceptor)
+            .addInterceptor(versionInterceptor)
+            .build()
+    }
 
-  @Provides
-  @Reusable
-  fun provideRetrofitInstance(
-    @BaseUrl baseUrl: String,
-    converterFactory: GsonConverterFactory,
-    @KaryaOkHttpClient okHttpClient: OkHttpClient
-  ): Retrofit {
-    return Retrofit.Builder().client(okHttpClient).baseUrl(baseUrl)
-      .addConverterFactory(converterFactory).build()
-  }
+    @Provides
+    @Reusable
+    fun provideRetrofitInstance(
+        @BaseUrl baseUrl: String,
+        converterFactory: GsonConverterFactory,
+        @KaryaOkHttpClient okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder().client(okHttpClient).baseUrl(baseUrl)
+            .addConverterFactory(converterFactory).build()
+    }
 
-  @Provides
-  @Reusable
-  fun provideLanguageAPI(retrofit: Retrofit): LanguageAPI {
-    return retrofit.create(LanguageAPI::class.java)
-  }
+    @Provides
+    @Reusable
+    fun provideLanguageAPI(retrofit: Retrofit): LanguageAPI {
+        return retrofit.create(LanguageAPI::class.java)
+    }
 
-  @Provides
-  @Reusable
-  fun provideMicroTaskAPI(retrofit: Retrofit): MicroTaskAssignmentAPI {
-    return retrofit.create(MicroTaskAssignmentAPI::class.java)
-  }
+    @Provides
+    @Reusable
+    fun provideMicroTaskAPI(retrofit: Retrofit): MicroTaskAssignmentAPI {
+        return retrofit.create(MicroTaskAssignmentAPI::class.java)
+    }
 
-  @Provides
-  @Reusable
-  fun provideWorkerAPI(retrofit: Retrofit): WorkerAPI {
-    return retrofit.create(WorkerAPI::class.java)
-  }
+    @Provides
+    @Reusable
+    fun provideWorkerAPI(retrofit: Retrofit): WorkerAPI {
+        return retrofit.create(WorkerAPI::class.java)
+    }
 
-  @Provides
-  @Reusable
-  fun provideKaryaFileAPIService(retrofit: Retrofit): KaryaFileAPI {
-    return retrofit.create(KaryaFileAPI::class.java)
-  }
+    @Provides
+    @Reusable
+    fun provideKaryaFileAPIService(retrofit: Retrofit): KaryaFileAPI {
+        return retrofit.create(KaryaFileAPI::class.java)
+    }
+
+    @Provides
+    @Reusable
+    fun providePaymentAPIService(retrofit: Retrofit): PaymentAPI {
+        return retrofit.create(PaymentAPI::class.java)
+    }
 }
