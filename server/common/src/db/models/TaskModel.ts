dc1d386374d4a2e79e63948fcf5445f108ef1832
Ananya S <ananya.saxena9716@gmail.com>
2022-02-22 13:16:35 +0530

    Modified task summary function to include scenario-specific task data in the summary

--- server/common/src/db/models/TaskModel.ts
+++ server/common/src/db/models/TaskModel.ts
@@ -9,14 +9,27 @@ import { knex } from '../Client';
 export async function tasksSummary(): Promise<any[]> {
   const response = await knex.raw(`
     SELECT
-      task_id,
-      COALESCE(SUM((status='ASSIGNED' OR status='COMPLETED' OR status='VERIFIED')::int), 0) as assigned,
-      COALESCE(SUM((status='COMPLETED' OR status='VERIFIED')::int), 0) as completed,
-      COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
-      COALESCE(SUM(credits), 0) as cost
-    FROM
-      microtask_assignment
-    GROUP BY task_id;
+      t.id,
+      t.scenario_name,
+      COALESCE((mta.assigned)::int, 0) as assigned,
+      COALESCE((mta.completed)::int, 0) as completed,
+      COALESCE((mta.verified)::int, 0) as verified,
+      COALESCE((mta.cost)::int, 0) as cost
+    FROM 
+      task as t
+    LEFT JOIN 
+      (
+        SELECT
+          task_id,
+          COALESCE(SUM((status='ASSIGNED' OR status='COMPLETED' OR status='VERIFIED')::int), 0) as assigned,
+          COALESCE(SUM((status='COMPLETED' OR status='VERIFIED')::int), 0) as completed,
+          COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
+          COALESCE(SUM(credits), 0) as cost
+        FROM
+          microtask_assignment
+        GROUP BY task_id
+      ) as mta
+    ON t.id = mta.task_id
     `);
 
   return response.rows.map((row: any) => {
