c591e8045e7927ad677dc2ff41d9eb785ea1b7a0
Ananya S <ananya.saxena9716@gmail.com>
2022-02-24 12:54:12 +0530

    Added function to get task data for text translation validation scenario

--- server/backend/src/scenarios/scenarios/TextTranslationValidation.ts
+++ server/backend/src/scenarios/scenarios/TextTranslationValidation.ts
@@ -45,51 +45,55 @@ async function processInputFile(
 }
 
 // Backend text translation validation scenario
-export const backendTextTranslationValidationScenario: IBackendScenarioInterface<BaseTextTranslationValidationScenario> =
-  {
-    ...baseTextTranslationValidationScenario,
-    processInputFile,
+export const backendTextTranslationValidationScenario: IBackendScenarioInterface<BaseTextTranslationValidationScenario> = {
+  ...baseTextTranslationValidationScenario,
+  processInputFile,
 
-    /**
-     * Generate output for text translation validation. For each verified assignment,
-     * generate JSON with the source sentence, translated sentence, score and
-     * verification report.
-     *
-     * TODO: output format should be made a task parameter.
-     */
-    async generateOutput(task, assignments, microtasks, task_folder, timestamp) {
-      const files = await BBPromise.mapSeries(assignments, async (assignment) => {
-        // Get the microtask record
-        const mt = (await BasicModel.getSingle('microtask', {
-          id: assignment.microtask_id,
-        })) as MicrotaskRecordType<'TEXT_TRANSLATION'>;
+  /**
+   * Generate output for text translation validation. For each verified assignment,
+   * generate JSON with the source sentence, translated sentence, score and
+   * verification report.
+   *
+   * TODO: output format should be made a task parameter.
+   */
+  async generateOutput(task, assignments, microtasks, task_folder, timestamp) {
+    const files = await BBPromise.mapSeries(assignments, async (assignment) => {
+      // Get the microtask record
+      const mt = (await BasicModel.getSingle('microtask', {
+        id: assignment.microtask_id,
+      })) as MicrotaskRecordType<'TEXT_TRANSLATION'>;
 
-        // Generate JSON data
-        const jsonData = {
-          source: mt.input.data.sentence,
-          score: assignment.output!.data.score,
-          worker_id: assignment.worker_id,
-          report: assignment.report,
-          credits: assignment.credits,
-        };
+      // Generate JSON data
+      const jsonData = {
+        source: mt.input.data.sentence,
+        score: assignment.output!.data.score,
+        worker_id: assignment.worker_id,
+        report: assignment.report,
+        credits: assignment.credits,
+      };
 
-        // Write the json data to file
-        const jsonFile = `${assignment.id}.json`;
-        await fsp.writeFile(`${task_folder}/${jsonFile}`, JSON.stringify(jsonData, null, 2) + '\n');
+      // Write the json data to file
+      const jsonFile = `${assignment.id}.json`;
+      await fsp.writeFile(`${task_folder}/${jsonFile}`, JSON.stringify(jsonData, null, 2) + '\n');
 
-        return jsonFile;
-      });
+      return jsonFile;
+    });
 
-      return files;
-    },
+    return files;
+  },
 
-    /**
-     * Text translation validation microtask output
-     * TODO: Temporarily returning null, as microtask output can be generated
-     * directly from the task level output and is typically not necessary for
-     * chaining.
-     */
-    async microtaskOutput(task, microtask, assignments) {
-      return null;
-    },
-  };
+  /**
+   * Text translation validation microtask output
+   * TODO: Temporarily returning null, as microtask output can be generated
+   * directly from the task level output and is typically not necessary for
+   * chaining.
+   */
+  async microtaskOutput(task, microtask, assignments) {
+    return null;
+  },
+
+  async getTaskData(task_id) {
+    const ob = {} as object;
+    return ob;
+  },
+};
