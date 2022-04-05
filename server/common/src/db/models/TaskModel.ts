1876ab5b0779068c6545cbb8f9720affc4aa68e9
Ananya S <ananya.saxena9716@gmail.com>
2022-04-05 20:49:23 +0530

    Modified task model and worker model functions to use materialized views

--- server/common/src/db/models/TaskModel.ts
+++ server/common/src/db/models/TaskModel.ts
@@ -6,31 +6,11 @@ import { knex } from '../Client';
 /**
  * Get summary info for all tasks
  */
-export async function tasksSummary(): Promise<any[]> {
-  const response = await knex.raw(`
-    SELECT
-      t.id,
-      t.scenario_name,
-      COALESCE((mta.assigned)::int, 0) as assigned,
-      COALESCE((mta.completed)::int, 0) as completed,
-      COALESCE((mta.verified)::int, 0) as verified,
-      COALESCE((mta.cost)::int, 0) as cost
-    FROM 
-      task as t
-    LEFT JOIN 
-      (
-        SELECT
-          task_id,
-          COALESCE(SUM((status='ASSIGNED' OR status='COMPLETED' OR status='VERIFIED')::int), 0) as assigned,
-          COALESCE(SUM((status='COMPLETED' OR status='VERIFIED')::int), 0) as completed,
-          COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
-          COALESCE(SUM(credits), 0) as cost
-        FROM
-          microtask_assignment
-        GROUP BY task_id
-      ) as mta
-    ON t.id = mta.task_id
-    `);
+export async function tasksSummary(force_refresh?: string): Promise<any[]> {
+  if (force_refresh) {
+    await knex.raw(`REFRESH MATERIALIZED VIEW CONCURRENTLY task_summary`);
+  }
+  const response = await knex.raw(`SELECT * FROM task_summary`);
 
   return response.rows.map((row: any) => {
     const { assigned, completed, verified, cost, ...rest } = row;
--- server/common/src/db/models/WorkerModel.ts
+++ server/common/src/db/models/WorkerModel.ts
@@ -3,34 +3,11 @@ import { knex } from '../Client';
 /**
  * Get summary info of all workers
  */
-export async function workersSummary(): Promise<any[]> {
-  const response = await knex.raw(`
-    SELECT
-      w.*,
-      COALESCE((mta.assigned + mta.completed + mta.verified)::int, 0) as assigned,
-      COALESCE((mta.completed + mta.verified)::int, 0) as completed,
-      COALESCE((mta.verified)::int, 0) as verified,
-      COALESCE((mta.earned)::int, 0) as earned,
-      (mta.earliest) as earliest,
-      (mta.latest) as latest
-    FROM 
-      worker as w
-    LEFT JOIN
-    (
-      SELECT
-        worker_id,
-        COALESCE(SUM((status='ASSIGNED')::int), 0) as assigned,
-        COALESCE(SUM((status='COMPLETED')::int), 0) as completed,
-        COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
-        COALESCE(SUM(credits), 0) as earned,
-        MIN(created_at) as earliest,
-        MAX(completed_at) as latest
-      FROM
-        microtask_assignment
-      GROUP BY worker_id
-    ) as mta
-  ON w.id = mta.worker_id
-    `);
+export async function workersSummary(force_refresh?: string): Promise<any[]> {
+  if (force_refresh) {
+    await knex.raw(`REFRESH MATERIALIZED VIEW CONCURRENTLY worker_summary`);
+  }
+  const response = await knex.raw(`SELECT * FROM worker_summary`);
 
   return response.rows.map((row: any) => {
     const { assigned, completed, verified, earned, earliest, latest, ...rest } = row;
@@ -42,33 +19,14 @@ export async function workersSummary(): Promise<any[]> {
 /**
  * Get summary info of workers for a particular task
  */
-export async function workersTaskSummary(task_id: string): Promise<any[]> {
+export async function workersTaskSummary(task_id: string, force_refresh?: string): Promise<any[]> {
+  if (force_refresh) {
+    await knex.raw(`REFRESH MATERIALIZED VIEW CONCURRENTLY worker_task_summary`);
+  }
   const response = await knex.raw(`
-    SELECT
-      w.*,
-      COALESCE((mta.assigned + mta.completed + mta.verified)::int, 0) as assigned,
-      COALESCE((mta.completed + mta.verified)::int, 0) as completed,
-      COALESCE((mta.verified)::int, 0) as verified,
-      COALESCE((mta.earned)::int, 0) as earned
-    FROM 
-      worker as w
-    LEFT JOIN
-    (
-      SELECT
-        worker_id,
-        COALESCE(SUM((status='ASSIGNED')::int), 0) as assigned,
-        COALESCE(SUM((status='COMPLETED')::int), 0) as completed,
-        COALESCE(SUM((status='VERIFIED')::int), 0) as verified,
-        COALESCE(SUM(credits), 0) as earned
-      FROM
-        microtask_assignment
-      WHERE
-        task_id = ${task_id}
-      GROUP BY worker_id
-    ) as mta
-    ON w.id = mta.worker_id
-    WHERE
-      mta.assigned > 0 OR mta.completed > 0 OR mta.verified > 0
+  SELECT * FROM worker_task_summary
+  WHERE
+    task_id = ${task_id}
     `);
 
   return response.rows.map((row: any) => {
