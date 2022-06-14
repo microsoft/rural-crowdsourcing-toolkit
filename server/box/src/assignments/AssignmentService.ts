7c08dd3ef2d0cb155495e5afa0b885ea28046b54
Vivek Seshadri <visesha@microsoft.com>
2022-06-14 10:58:20 +0530

    Feature: task deadline -- transmitting microtask deadlines to corresponding assignments

--- server/box/src/assignments/AssignmentService.ts
+++ server/box/src/assignments/AssignmentService.ts
@@ -171,6 +171,7 @@ export async function assignMicrotasksForWorker(worker: WorkerRecord, maxCredits
         task_id: task.id,
         microtask_id: microtask.id,
         worker_id: worker.id,
+        deadline: microtask.deadline,
         wgroup: worker.wgroup,
         max_credits: microtask.credits,
         status: 'ASSIGNED',
