0af55fe2604243c8b7d4af1d2c5056327f95dbdb
Vivek Seshadri <visesha@microsoft.com>
2022-06-28 13:56:28 +0530

    registered_at should not change if the user uninstalls and reinstalls the app.

--- server/box/src/controllers/WorkerController.ts
+++ server/box/src/controllers/WorkerController.ts
@@ -91,10 +91,12 @@ export const update: KaryaMiddleware = async (ctx) => {
 export const registerWorker: KaryaMiddleware = async (ctx) => {
   // extract relevant fields from worker.
   const now = new Date().toISOString();
+  const registered_at = ctx.state.entity.reg_mechanism == null ? now : ctx.state.entity.registered_at;
+
   const record = await BasicModel.updateSingle(
     'worker',
     { id: ctx.state.entity.id },
-    { reg_mechanism: 'phone-otp', registered_at: now, profile_updated_at: now }
+    { reg_mechanism: 'phone-otp', registered_at, profile_updated_at: now }
   );
   const id_token = ctx.state.entity.id_token;
   ctx.state.entity = { ...record, id_token };
