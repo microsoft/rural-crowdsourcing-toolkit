9ade59a5836ce87f827e53c25edd775c7fdd054a
Anurag Shukla <shukla.anurag0006@gmail.com>
2022-09-12 18:16:48 +0530

    Delete old code to sync first time

--- client/app/src/main/java/com/microsoft/research/karya/ui/dashboard/DashboardFragment.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/dashboard/DashboardFragment.kt
@@ -227,14 +227,6 @@ class DashboardFragment : SessionFragment(R.layout.fragment_dashboard) {
       (binding.tasksRv.adapter as TaskListAdapter).updateList(taskInfoData)
     }
 
-    // Check if worker is initialised in viewmodel
-    if (viewModel.workerAccessCode.value.isNotEmpty()) {
-      // Sync if no tasks are present
-      if (data.taskInfoData.isEmpty()) {
-        syncWithServer()
-      }
-    }
-
     // Show a dialog box to sync with server if completed tasks and internet available
     if (requireContext().isNetworkAvailable()) {
       for (taskInfo in data.taskInfoData) {
