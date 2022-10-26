b32d2fb8ca2f8740be8b01105b2404c637e17063
Vivek Seshadri <visesha@microsoft.com>
2022-10-26 12:03:31 -0500

    Updated the pay rates for task variants

--- server/backend/src/scripts/CreateRaniTaskVariants.ts
+++ server/backend/src/scripts/CreateRaniTaskVariants.ts
@@ -9,12 +9,38 @@ dotenv.config();
 import { knex, setupDbConnection, setupBlobStore, BasicModel } from '@karya/common';
 import { promises as fsp } from 'fs';
 import { processInputFile } from '../task-ops/ops/InputProcessor';
-import { TaskRecordType } from '@karya/core';
+import { ScenarioName, TaskRecordType } from '@karya/core';
+
+type PayType = 'low' | 'med' | 'high';
+
+// @ts-ignore
+const taskPayRates: { [id in ScenarioName]: { [id in PayType]: number } } = {
+  IMAGE_ANNOTATION: {
+    low: 0.7,
+    med: 1.85,
+    high: 3.6,
+  },
+  SPEECH_DATA: {
+    low: 0.2,
+    med: 0.55,
+    high: 1.2,
+  },
+  SPEECH_TRANSCRIPTION: {
+    low: 1.1,
+    med: 2.21,
+    high: 4.38,
+  },
+  SENTENCE_CORPUS: {
+    low: 10,
+    med: 25,
+    high: 50,
+  },
+};
 
 const task_id = process.argv[2];
 const var_type_arg = process.argv[3];
 
-const variant_type = var_type_arg == 'med' ? 'med' : var_type_arg == 'high' ? 'high' : null;
+const variant_type: PayType | null = var_type_arg == 'med' ? 'med' : var_type_arg == 'high' ? 'high' : null;
 if (variant_type == null) {
   throw 'Invalid variant type';
 }
@@ -26,29 +52,16 @@ if (variant_type == null) {
   const lowTask = (await BasicModel.getSingle('task', { id: task_id })) as TaskRecordType;
   const { id, name, params, itags, created_at, last_updated_at, ...rest } = lowTask;
 
-  if (variant_type == 'med') {
-    const medParams = { ...params };
-    medParams.baseCreditsPerMicrotask = params.baseCreditsPerMicrotask * 2;
-    medParams.creditsPerMicrotask = params.creditsPerMicrotask * 2;
+  const payRate = taskPayRates[lowTask.scenario_name][variant_type];
+  const newParams = { ...params };
+  newParams.baseCreditsPerMicrotask = payRate * 0.75;
+  newParams.creditsPerMicrotask = payRate * 0.25;
 
-    const medTags = itags.itags.filter((t) => t != 'pay-low');
-    medTags.push('pay-med');
+  const newTags = itags.itags.filter((t) => t != 'pay-low');
+  newTags.push(`pay-${variant_type}`);
 
-    const medName = name.replace('Low', 'Med');
+  const newName = name.replace('Low', variant_type == 'high' ? 'High' : 'Med');
 
-    const medTask = { ...rest, name: medName, params: medParams, itags: { itags: medTags } };
-    await BasicModel.insertRecord('task', medTask);
-  } else if (variant_type == 'high') {
-    const highParams = { ...params };
-    highParams.baseCreditsPerMicrotask = params.baseCreditsPerMicrotask * 4;
-    highParams.creditsPerMicrotask = params.creditsPerMicrotask * 4;
-
-    const highTags = itags.itags.filter((t) => t != 'pay-low');
-    highTags.push('pay-high');
-
-    const highName = name.replace('Low', 'High');
-
-    const highTask = { ...rest, name: highName, params: highParams, itags: { itags: highTags } };
-    await BasicModel.insertRecord('task', highTask);
-  }
+  const newTask = { ...rest, name: newName, params: newParams, itags: { itags: newTags } };
+  await BasicModel.insertRecord('task', newTask);
 })().finally(() => knex.destroy());
