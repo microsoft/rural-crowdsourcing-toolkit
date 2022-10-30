68426c393e0a56120e6cb4c10940e3c88abe4082
Anurag Shukla <shukla.anurag0006@gmail.com>
2022-10-30 20:02:24 +0530

    Fix payments transaction query to send data to frontend

--- server/common/src/db/models/PaymentsTransactionModel.ts
+++ server/common/src/db/models/PaymentsTransactionModel.ts
@@ -4,7 +4,7 @@ import { knex } from '../Client';
  *
  * Get payments detail for frontend
  */
-export async function getPaymentsTransactionData(worker_id: string): Promise<any[]> {
+export async function getPaymentsTransactionData(): Promise<any[]> {
     const response = await knex.raw(`SELECT tp.*, tw.unique_id FROM 
     (select * from payments_transaction) tp 
     inner join (select id, extras->>'unique_id' as unique_id from worker) tw 
