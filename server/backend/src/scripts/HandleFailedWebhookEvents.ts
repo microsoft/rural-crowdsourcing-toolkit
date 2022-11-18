161c6fe8b953d4fc991acd33edc59f0761c5b7e7
Vivek Seshadri <visesha@microsoft.com>
2022-11-18 03:51:08 -0600

    Fix: CLI to handle webhooks cannot handle ones without payout_id

--- server/backend/src/scripts/HandleFailedWebhookEvents.ts
+++ server/backend/src/scripts/HandleFailedWebhookEvents.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 //
-// Update transaction record status for the payouts that were created but 
+// Update transaction record status for the payouts that were created but
 // their webhooks couldn't be received.
 
 import dotenv from 'dotenv';
@@ -11,21 +11,27 @@ import { knex, setupDbConnection, setupBlobStore, BasicModel } from '@karya/comm
 import { FINAL_TRANSACTION_STATES, PaymentsTransactionRecord, PayoutResponse } from '@karya/core';
 import { razorPayAxios } from '../Queue/HttpUtils';
 import { updateTransactionRecord } from '../webhook-controllers/payments/RazorpayController';
+import { Promise as BBPromise } from 'bluebird';
 
 const RAZORPAY_CONTACTS_RELATIVE_URL = 'payouts';
 
 /** Main Script */
 (async () => {
   setupDbConnection();
-  const formattedFinalStates = FINAL_TRANSACTION_STATES.map((state, idx, arr) => `'${state}'`)
+  const formattedFinalStates = FINAL_TRANSACTION_STATES.map((state, idx, arr) => `'${state}'`);
   // get records with non final states
-  const knexResponse = await knex.raw(`SELECT * from payments_transaction WHERE STATUS NOT IN (${formattedFinalStates})`)
-  const transactionRecords: PaymentsTransactionRecord[] = knexResponse.rows
+  const knexResponse = await knex.raw(
+    `SELECT * from payments_transaction WHERE STATUS NOT IN (${formattedFinalStates})`
+  );
+  const transactionRecords: PaymentsTransactionRecord[] = knexResponse.rows;
 
-  for (const transactionRecord of transactionRecords) {
-    const payoutId = transactionRecord.id
-    const razorpayResponse = await razorPayAxios.get<PayoutResponse>(`${RAZORPAY_CONTACTS_RELATIVE_URL}/${transactionRecord.payout_id}`)
-    await updateTransactionRecord(razorpayResponse.data)
-  }
-
+  await BBPromise.mapSeries(transactionRecords, async (transactionRecord) => {
+    const payout_id = transactionRecord.payout_id;
+    if (payout_id == null) {
+      console.log(`Cannot update transaction ${transactionRecord.id}: No payout ID`);
+      return;
+    }
+    const razorpayResponse = await razorPayAxios.get<PayoutResponse>(`${RAZORPAY_CONTACTS_RELATIVE_URL}/${payout_id}`);
+    await updateTransactionRecord(razorpayResponse.data);
+  });
 })().finally(() => knex.destroy());
