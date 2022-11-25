8511f8975ca5d9c551b689af71a81b695a34616c
Vivek Seshadri <visesha@microsoft.com>
2022-11-25 10:45:29 -0800

    Update profile and registered_at information on profile update

--- server/backend/src/box-routes-controller/WorkerController.ts
+++ server/backend/src/box-routes-controller/WorkerController.ts
@@ -51,11 +51,30 @@ export const updateWorkers: BoxRouteMiddleware = async (ctx) => {
 
   await BBPromise.mapSeries(workers, async (worker) => {
     try {
-      const { phone_number, reg_mechanism, year_of_birth, gender, full_name, language } = worker;
+      const {
+        phone_number,
+        reg_mechanism,
+        year_of_birth,
+        gender,
+        full_name,
+        language,
+        profile,
+        registered_at,
+      } = worker;
       await BasicModel.updateSingle(
         'worker',
         { id: worker.id },
-        { phone_number, reg_mechanism, year_of_birth, gender, full_name, language, sent_to_server_at }
+        {
+          phone_number,
+          reg_mechanism,
+          year_of_birth,
+          gender,
+          full_name,
+          language,
+          sent_to_server_at,
+          profile,
+          registered_at,
+        }
       );
       response.push({ id: worker.id, sent_to_server_at });
     } catch (e) {
