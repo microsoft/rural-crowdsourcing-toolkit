0564976d353cb997b7962c830a8d09959e6351ca
Vivek Seshadri <vivek@karya.in>
2023-01-11 08:10:11 -0600

    Multiple workers in the preassign script

--- server/box/src/scripts/PreassignTasks.ts
+++ server/box/src/scripts/PreassignTasks.ts
@@ -7,15 +7,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { knex, setupDbConnection, BasicModel } from '@karya/common';
+import { promises as fsp } from 'fs';
 import { preassignMicrotasksForWorker } from '../assignments/AssignmentService';
+import { Promise as BBPromise } from 'bluebird';
 
-const worker_id = process.argv[2];
+const worker_id_file = process.argv[2];
 
 /** Main Script */
 (async () => {
   setupDbConnection();
-  const worker = await BasicModel.getSingle('worker', { id: worker_id });
-  await preassignMicrotasksForWorker(worker, 10000);
+
+  const worker_ids_data = await fsp.readFile(worker_id_file);
+  const worker_ids = worker_ids_data.toString().split('\n');
+
+  await BBPromise.mapSeries(worker_ids, async (worker_id) => {
+    console.log(new Date().toISOString(), worker_id);
+    const worker = await BasicModel.getSingle('worker', { id: worker_id });
+    await preassignMicrotasksForWorker(worker, 10000);
+    console.log(new Date().toISOString(), worker_id);
+  });
 })()
   .catch((e) => {
     console.log(e);
