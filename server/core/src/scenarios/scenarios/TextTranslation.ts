6709871ae06b61c3baae827469d7ba54ea06a9d6
Anurag Shukla <shukla.anurag0006@gmail.com>
2023-01-12 19:58:08 +0530

    Add: BOW as a microtask parameter

--- server/core/src/scenarios/scenarios/TextTranslation.ts
+++ server/core/src/scenarios/scenarios/TextTranslation.ts
@@ -81,8 +81,11 @@ const task_input: BaseTextTranslationScenario['task_input'] = [
 const task_input_file: BaseTextTranslationScenario['task_input_file'] = {
   json: {
     required: true,
-    description: `JSON file containing an array of objects. Each object must have a sentence field that contains the sentence prompt for the translation and a providedTranslation field that contains a translation which needs an edit, provide an empty string to this field to provide no translation.`,
-    schema: Joi.array().items(Joi.object({ sentence: Joi.string(), providedTranslation: Joi.string() }).unknown(true)),
+    description: `JSON file containing an array of objects. 
+    Each object must have a sentence field that contains the sentence prompt for the translation, \n 
+    a providedTranslation field that contains a translation which needs an edit, provide an empty string to this field to provide no translation, \n
+    and a bow field that contains a sentence, words for which will be used to provide with static bow assistance`,
+    schema: Joi.array().items(Joi.object({ sentence: Joi.string(), providedTranslation: Joi.string(), bow: Joi.string() }).unknown(true)),
   },
   tgz: { required: false },
 };
